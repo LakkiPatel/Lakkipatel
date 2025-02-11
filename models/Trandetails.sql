5d8edb3306c27f3d786af0408a4942cfd610a1ba
Lakki patel <lakki.patel@corecard.com>
2025-02-11 16:22:32 +0530

    update file trandetails

--- models/Trandetails.sql
+++ models/Trandetails.sql
@@ -6,7 +6,7 @@
     enabled = true
 )}}
 
-with Trans as (
+with Tran as (
     select
     tranid,
     Customername,
@@ -26,7 +26,7 @@ Customer as (
         From {{ref('Customer_view') }}
 ),
 final as (
-    select * from Trans t join Customer c on (t.Customername=c.name)  
+    select * from Tran t join Customer c on (t.Customername=c.name)  
 {% if is_incremental() %}
 {% do log("lakki", info=True) %}
     WHERE 
